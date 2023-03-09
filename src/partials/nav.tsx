72afc43b91a1cafc8da5ddabda22c62222c4d905
mqtech25 <mqtech25@gmail.com>
2023-03-09 16:46:21 +0500

    Nav changes

--- src/partials/nav.tsx
+++ src/partials/nav.tsx
@@ -1,31 +1,74 @@
 import logo from "../logo.png";
+import { NavLink } from "react-router-dom";
 import * as icon from "react-icons/fa";
 const Nav = () => {
   return (
     <>
-      <div className="grid grid-cols-1 md:grid-cols-4 bg-[#242424]">
+      {/* <div className="grid grid-cols-1 md:grid-cols-4 bg-[#242424]">
         <div className="flex justify-evenly items-center  p-3">
           <img src={logo} alt="" />
           <i className="text-white">{<icon.FaBars size={28} />}</i>
         </div>
         <div className=" col-span-2">
-          <div className="grid md:grid-cols-2">
-            <input
-              type="text"
-              className="px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 disabled:shadow-none"
-              name="name"
-            />
-            <nav className="list">
-              <ul>
-                <li>Item1</li>
-                <li>Item2</li>
-                <li>Item3</li>
-              </ul>
-            </nav>
-          </div>
+          <input type="text" className="px-3 py-2" name="name" />
+          <nav className="list">
+            <ul>
+              <li>Item1</li>
+              <li>Item2</li>
+              <li>Item3</li>
+            </ul>
+          </nav>
         </div>
         <div className="flex justify-center ">Cart</div>
-      </div>
+      </div> */}
+      <nav className="bg-dark-color text-white dark-shadow">
+        <div className="flex w-11/12 mx-auto justify-between  h-20">
+          <div className="w-full md:w-80 brand-logo flex items-center justify-evenly">
+            <img src={logo} alt="" />
+          </div>
+          <div className="flex-1 hidden lg:block nav ">
+            <div className="navbar flex items-center justify-around h-full">
+              <div className="nav__icon">
+                <i className="text-white">{<icon.FaBars size={28} />}</i>
+              </div>
+              <div className="nav__input">
+                <input type="text" name="" id="" />
+              </div>
+              <div className="nav__links">
+                <ul className="primary__nav list-none flex gap-6">
+                  <li>
+                    <NavLink
+                      to="/"
+                      className={({ isActive }) =>
+                        isActive
+                          ? "active text-[#dfc500]  font-[600]"
+                          : "hover:text-[#dfc500] cursor-pointer transition-all"
+                      }
+                    >
+                      Home
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to="/about"
+                      className={({ isActive }) =>
+                        isActive
+                          ? "active text-[#dfc500]  font-[600]"
+                          : "hover:text-[#dfc500] cursor-pointer transition-all"
+                      }
+                    >
+                      About
+                    </NavLink>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="w-full md:w-80 tools flex items-center justify-center">
+            connect
+          </div>
+        </div>
+      </nav>
     </>
   );
 };
